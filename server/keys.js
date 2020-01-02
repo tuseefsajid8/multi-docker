6044ec015b3e015c4298b1e6b28f379cbe9b41e1
Ubuntu <ubuntu@ip-172-31-11-76.us-east-2.compute.internal>
2020-01-02 05:10:29 +0000

    initial commit

---
+++ server/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.PGUSER,
+  psHost: process.env.PGHOST,
+  psDatabase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT
+	 
+};
+
